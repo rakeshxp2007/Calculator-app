22304b5201dbc853e623fa1a1a183e8c42d0d368
Rakesh Kumar Sahoo <electromarine@Rakeshs-Mac-mini.local>
2025-10-04 22:08:21 +0530

    removed multiplication and division functionality

--- calc_choices.py
+++ calc_choices.py
@@ -7,11 +7,3 @@ def do_addition(a: float, b: float) -> float:
 def do_subtraction(a: float, b: float) -> float:
     """Subtract second number from first and return the result."""
     return a - b
-
-# # Multiplication
-# def do_multiplication(a: float, b: float) -> float:
-#     """Multiply two numbers and return the result."""
-#     return a * b
-
-# 
-    
